8512762e6f52b644398f64a89f6722df48732363
Arindum Shree <shreearindum2004@gmail.com>
2026-08-27 23:46:18 +0530

    Easy-0 ms (100%)-19.3 MB (17.55%)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def solve(self,n,dp):
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        if dp[n]!=-1:
+            return dp[n]
+        dp[n]=self.solve(n-1,dp)+self.solve(n-2,dp)
+        return dp[n]
+    def climbStairs(self, n: int) -> int:
+        dp=[-1]*(n+1)
+        return self.solve(n,dp)
