d680d2a5431b5a767d52e5bfaa5ce29aa98ec13e
Arindum Shree <shreearindum2004@gmail.com>
2026-08-27 11:52:06 +0530

    Medium-0 ms (100%)-20.4 MB (74.74%)

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, n: int, prerequisites: List[List[int]]) -> List[int]:
+        adjList=[]*n
+        ans=[]
+        incoming=[0]*n
+        q=[]
+        for i in range(n):
+            adjList.append([])
+        for a,b in prerequisites:
+            incoming[a]+=1
+            adjList[b].append(a)
+        for i in range(n):
+            if incoming[i]==0:
+                ans.append(i)
+                q.append(i)
+        while q:
+            front=q.pop(0)
+            for x in adjList[front]:
+                incoming[x]-=1
+                if incoming[x]==0:
+                    ans.append(x)
+                    q.append(x)
+        if len(ans)==n:
+            return ans
+        else:
+            return []
