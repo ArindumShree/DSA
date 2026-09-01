dc8180d28ab83e28f70647b95a27be0ccdb23b21
Arindum Shree <shreearindum2004@gmail.com>
2026-09-02 03:45:20 +0530

    Easy-0 ms (100%)-19.3 MB (18.97%)

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,7 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         n=len(nums)
-        b=n
-        for i in range(b-1,-1,-1):
-            if nums[i]==val:
-                nums.pop(i)
+        # b=n
+        # for i in range(b-1,-1,-1):
+        #     if nums[i]==val:
+        #         nums.pop(i)
+        k=0
+        for i in range(n):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k
